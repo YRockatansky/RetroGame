6cb40f2c8c0ad4a211ae8e7d2b6ad4bf05cc2130
Jorge Galarza <jorgazam@teleco.upv.es>
2019-04-25 17:22:24 +0200

    First commit

---
+++ RetroGame/Assets/Scripts/DemoScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour {
+
+    public GameObject[] ObjectPool;
+    public GameObject Sun;
+    public float floatSpeed = 1f;
+    public Transform RefreshPivot;
+    public Transform TerrainRefreshPivot;
+    public float speed = 1f;
+
+    // Adjust speed over time. Make game harder and harder each 10 seconds!
+    public void Start()
+    {
+        InvokeRepeating("IncreaseSpeed", 10f, 10f);
+    }
+
+
+    // Make them closer each second. Let them come next to camera
+    void Update () {
+        foreach(GameObject ob in ObjectPool)
+        {
+            ob.transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        Sun.transform.Translate(Vector3.up * Time.deltaTime * floatSpeed * Mathf.Sin(Time.time) * 0.2f); 
+    }
+
+    // Super simple..
+    public void IncreaseSpeed()
+    {
+        speed += 20f;
+    }
+
+    // Make sure that your terrain marked with layer 'Terrain' - Number 24
+    // Position changing stuff
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 24) // If terrain use the TerrainRefreshPoint
+        {
+            other.gameObject.transform.position = new Vector3(other.gameObject.transform.position.x, other.gameObject.transform.position.y, TerrainRefreshPivot.transform.position.z);
+        }
+        else if(other.gameObject.layer == 9) { // We have a bad boy.. I mean, we have an enemy.. Or I don't know.. Innocent retro driver that we shouldn't crash? It's your call..
+            int RandomPosition = Random.Range(0, 3) - 1; // Generate a random number to set random lane
+            Vector3 newPosition = new Vector3(RefreshPivot.transform.position.x + (RandomPosition * 10), RefreshPivot.transform.position.y, RefreshPivot.transform.position.z);
+            other.gameObject.transform.position = newPosition;
+        }
+        else
+        { // If tree..
+            other.gameObject.transform.position = RefreshPivot.transform.position;
+        }
+    }
+
+}
---
+++ RetroGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Usually, I wouldn't use any Menu handler in player controller. But we needed to handle this quickly. And I didn't want to make things complicated for you
+/// So, this is for nearly whole player controller.
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    // Menu handlers
+    public GameObject GameOverMenu;
+    public GameObject StartGameMenu;
+    public GameObject MusicPlayer;
+
+    // Private 
+    private int Lane;
+    private Vector3 BaselineStartPosition;
+    private Vector3 targetVector;
+
+    // Required to avoid key bouncing caused by your keyboard.
+    private bool triggerLock = false;
+    
+    // Set predefined values
+    void Start()
+    {
+        Time.timeScale = 0;  // Stop time..
+        Lane = 1;                // Start location is 1.  Means middle lane.
+        BaselineStartPosition = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+        targetVector = BaselineStartPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Always try to change the lane. If target is same, nothing will happen anyway.
+        this.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, targetVector, 20f * Time.deltaTime);
+        // Cheesy drift animation..
+        this.transform.LookAt(new Vector3(targetVector.x, targetVector.y, targetVector.z + 35f) );
+
+        // If position is same, remove the lock.
+        if (this.transform.position == targetVector)
+        {
+            triggerLock = false;
+        }
+
+        // If locked, don't do anything. It can create some unwanted problems.
+        if (!triggerLock)
+        {
+            // If left
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (Lane > 0)
+                {
+                    // Decrease lane and move player. Also, lock the input recieving stuff
+                    Lane--;
+                    movePlayer();
+                    triggerLock = true;
+                }
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                if (Lane < 2)
+                {
+                    Lane++;
+                    movePlayer();
+                    triggerLock = true;
+                }
+            }
+        }
+    }
+
+    private void movePlayer()
+    {
+        // Change lane. It basically means changin the target location. Moving is handled from the update field anyway
+        switch (Lane)
+        {
+            case 0:
+                targetVector = new Vector3(BaselineStartPosition.x - 10, BaselineStartPosition.y, BaselineStartPosition.z);
+                break;
+            case 1:
+                targetVector = new Vector3(BaselineStartPosition.x, BaselineStartPosition.y, BaselineStartPosition.z);
+                break;
+            case 2:
+                targetVector = new Vector3(BaselineStartPosition.x + 10, BaselineStartPosition.y, BaselineStartPosition.z);
+                break;
+        }
+       // Debug.Log("Lane changed " + Lane);
+    }
+
+    // Go with game. Set menus inactive and make the time flow..
+    public void StartGame()
+    {
+        StartGameMenu.SetActive(false);
+        GameOverMenu.SetActive(false);
+        MusicPlayer.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    // Well thats a bit tricky.
+    public void RestartGame()
+    {
+        // Reload the scene. Thats not what I do usually. But cheapeast way to achieve this. If you have some performance issues, go with commented section.
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        /*
+        StartGameMenu.SetActive(false);
+        GameOverMenu.SetActive(false);
+
+        Lane = 1;
+        this.transform.position = BaselineStartPosition;
+        targetVector = BaselineStartPosition;
+        Time.timeScale = 1;
+        */
+    }
+
+    // If enter, game over. No need to check. We know who entered..
+    public void OnTriggerEnter(Collider other)
+    {
+        GameOverMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
